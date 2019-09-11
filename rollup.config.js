2488712e1ce2a9800a4d0ba3c861913466f37d54
Alexandre Cesar <acmorcego@gmail.com>
2019-09-11 11:53:22 -0300

    add lit-html with node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
+import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
   output: {
+    name: 'main',
     file: 'dist/bundle.js',
     format: 'iife'
   },
-  plugins: [terser()]
+  plugins: [resolve(), terser()]
 }
