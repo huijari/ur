ce090a31d366a27563f3f10dacdd615971a8955f
Alexandre Cesar <acmorcego@gmail.com>
2019-09-11 10:18:48 -0300

    replace babel with rollup

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife'
+  },
+  plugins: [terser()]
+}
