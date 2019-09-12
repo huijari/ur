a81b3d3005cdb1c47464e1afdcdd105b9c501eb5
Alexandre Cesar <acmorcego@gmail.com>
2019-09-12 12:20:01 -0300

    show occupied cells

--- src/index.js
+++ src/index.js
@@ -20,35 +20,52 @@ const Player = (number, { turn, phase, players }) => {
   `
 }
 
-const Cell = name => html`
-  <div id=${name} class="Cell" style="grid-area: ${name};"></div>
+const Cell = (name, player) => html`
+  <div id=${name} class="Cell" style="grid-area: ${name};">
+    ${player}
+  </div>
 `
-const Board = ({ players }) => html`
-  <section class="Board">
-    ${[
-      'l0',
-      'l1',
-      'l2',
-      'l3',
-      'r0',
-      'r1',
-      'r2',
-      'r3',
-      'l12',
-      'l13',
-      'r12',
-      'r13',
-      'c4',
-      'c5',
-      'c6',
-      'c7',
-      'c8',
-      'c9',
-      'c10',
-      'c11'
-    ].map(Cell)}
-  </section>
-`
+const toBoard = (position, player) => {
+  if (position > 3 && position < 12) return `c${position}`
+  const side = player === 0 ? 'l' : 'r'
+  return `${side}${position}`
+}
+const Board = ({ players }) => {
+  const playerBoardPieces = player => (pieces, position) => ({
+    ...pieces,
+    [toBoard(position, player)]: player
+  })
+  const occupied = {
+    ...players[0].board.reduce(playerBoardPieces(0), {}),
+    ...players[1].board.reduce(playerBoardPieces(1), {})
+  }
+  return html`
+    <section class="Board">
+      ${[
+        'l0',
+        'l1',
+        'l2',
+        'l3',
+        'r0',
+        'r1',
+        'r2',
+        'r3',
+        'l12',
+        'l13',
+        'r12',
+        'r13',
+        'c4',
+        'c5',
+        'c6',
+        'c7',
+        'c8',
+        'c9',
+        'c10',
+        'c11'
+      ].map(cell => Cell(cell, occupied[cell]))}
+    </section>
+  `
+}
 
 const App = state => {
   const player1 = Player(0, state)
