23913bc1b48b1655116e0362671bce8a5f1d9e42
Alexandre Cesar <acmorcego@gmail.com>
2019-09-13 02:14:38 -0300

    write styles

--- src/component/cell.js
+++ src/component/cell.js
@@ -1,19 +1,24 @@
 import { html } from 'lit-html'
+import { classMap } from 'lit-html/directives/class-map'
 
 import { move } from '../actions'
 
 const Cell = (name, action, player, enabled) => {
   const handler = () => move(action)
-  const enabledClass = enabled ? 'enabled' : ''
+  const classes = {
+    Cell: true,
+    'Cell--enabled': enabled,
+    'Cell--bonus': ['l3', 'r3', 'c7', 'l13', 'r13'].includes(name),
+    'Cell--left': player === 0,
+    'Cell--right': player === 1
+  }
   return html`
     <div
       id=${name}
       @click=${handler}
-      class="Cell ${enabledClass}"
+      class="${classMap(classes)}"
       style="grid-area: ${name};"
-    >
-      ${player}
-    </div>
+    ></div>
   `
 }
 
--- src/component/player.js
+++ src/component/player.js
@@ -1,17 +1,34 @@
 import { html } from 'lit-html'
+import { classMap } from 'lit-html/directives/class-map'
 
 import { rollCoin } from '../actions'
+
+const Coin = value => {
+  const src = value === 0 ? 'tails' : 'heads'
+  return html`
+    <img src="./assets/coin_${src}.svg" />
+  `
+}
 
 const Player = (number, { turn, phase, players }) => {
   const player = players[number]
   const canRoll = turn === number && phase === 0
+  const side = number === 0 ? 'left' : 'right'
+  const classes = {
+    Player: true,
+    'Player--enabled': number === turn
+  }
   return html`
-    <section class="Player">
-      <h2>Player ${number + 1}</h2>
-      <p><b>Ascended:</b> ${player.pieces[1]}</p>
-      <p><b>On hand:</b> ${player.pieces[0]}</p>
-      <p>${player.coins}</p>
-      <button ?disabled=${!canRoll} @click=${rollCoin}>roll</button>
+    <section class=${classMap(classes)}>
+      <img src="./assets/player_${side}.svg" />
+      <div class="Player__stats">
+        <b>ascended: ${player.pieces[1]}</b>
+        <b>on hand: ${player.pieces[0]}</b>
+      </div>
+      <p>${player.coins.map(Coin)}</p>
+      <button ?disabled=${!canRoll} @click=${rollCoin} class="Player__roll">
+        roll
+      </button>
     </section>
   `
 }
